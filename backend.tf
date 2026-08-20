b4ae7d7332e89932be47bac552d0155f3b79e245
Lam Quoc Huy <huyqlam@kms-technology.com>
2026-08-20 16:44:07 +0700

    backend file

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "sttfstatemyapp123"   # đổi đúng tên bạn đã đặt được ở bước 2
+    container_name        = "tfstate"
+    key                    = "prod.terraform.tfstate"
+  }
+}

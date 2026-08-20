30e308fd1dae421b65d36c6b1d565bb7a7f06192
Lam Quoc Huy <huyqlam@kms-technology.com>
2026-08-20 16:55:21 +0700

    fix build

--- backend.tf
+++ backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "sttfstatemyapp123"   # đổi đúng tên bạn đã đặt được ở bước 2
     container_name        = "tfstate"
     key                    = "prod.terraform.tfstate"
+    use_azuread_auth = true
   }
 }
